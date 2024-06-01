1a0652962ae5f9419d8209f373523ceed0b28c37
imisgsc <simigracia@gmail.com>
2024-06-01 12:58:13 +0530

    completed level task 2

---
+++ LEVEL-1/TASK-2/script.js
@@ -0,0 +1,7 @@
+// Function to calculate and display sum
+function calculate() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var result = num1 + num2;
+    alert('The sum of ' + num1 + ' and ' + num2 + ' is ' + result);
+}
